5543208653438da6371ac3e656adac0cbd6c5bb8
SandyCreek <xiezhuoli771@gmail.com>
2024-03-13 16:15:44 -0500

    init

---
+++ apa-dashboard/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import TLACMap from './components/tlac-map.js';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to APA Dashboard!</h1>
+      <TLACMap />
+    </div>
+  );
+}
+
+export default App;
