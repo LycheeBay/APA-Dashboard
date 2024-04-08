9b5bc504a2644cb3210197a299e1ad6af206550c
SandyCreek <xiezhuoli771@gmail.com>
2024-04-08 00:29:58 -0500

    url locking works

--- apa-dashboard/src/components/building-info.js
+++ apa-dashboard/src/components/building-info.js
@@ -1,13 +1,35 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import {useState, useEffect} from 'react';
+
+// function filterData(data, 
 
 function BuildingInfo() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`http://127.0.0.1:5000/api/kennel/start=1&end=265`);
+      // console.log(response);
+      const newData = await response.json();
+      // console.log(newData);
+      setData(newData['data']);
+    };
+    fetchData();
+  }, []);
+
+  const currentUrl = window.location.href;
+  const urlParams = new URLSearchParams(window.location.search);
+  const keyword = urlParams.get('keyword');
+  const color = urlParams.get('color');
+  const location = urlParams.get('location');
+  console.log(keyword);
+
   return (
     <div className="BuildingInfo">
       <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>d</Card.Title>
           <Card.Text>
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
--- apa-dashboard/src/components/navbar.js
+++ apa-dashboard/src/components/navbar.js
@@ -5,8 +5,28 @@ import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
+import {useState} from 'react';
 
 function NavBar() {
+  const [color, setColor] = useState('All');
+  const [building, setBuilding] = useState('All');
+
+  // const history = useHistory();
+
+  const handleSearch = () => {
+    const url = `search?keyword=${document.getElementById('searchInput').value}&color=${color}&location=${building}`;
+    const pathnameParts = window.location.href.split('/');
+    console.log(pathnameParts);
+    if (pathnameParts[pathnameParts.length - 1].startsWith('search')) {
+      pathnameParts.pop();
+      const newUrl = pathnameParts.join('/');
+      window.location.href = `${newUrl}/${url}`;
+    }
+    else {
+      window.location.href = `/${url}`;
+    }
+  };
+
   return (
     <div className="NavBar">
       <link
@@ -29,26 +49,31 @@ function NavBar() {
       <Form>
         <Row className="g-0">
           <Col md={2} className="g-0">
-            <img src="/../data/apa-logo.png" alt="logo" />
+            <img src="../data/apa-logo.png" alt="logo" />
           </Col>
           <Col md={3} className="g-0">
-            <Form.Control placeholder="Disabled input" />
+            <Form.Control id="searchInput" placeholder="Enter a keyword" />
           </Col>
           <Col md={1} className="g-0">
-            <Button>Search</Button>
+            <Button onClick={handleSearch}>Search</Button>
           </Col>
           <Col md={3} className="g-0">
-            <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            <DropdownButton id="color-dropdown" title="Volunteer Category Color" onSelect={function(evt){console.log(evt); setColor(evt);}}>
+              <Dropdown.Item eventKey="All">All</Dropdown.Item>
+              <Dropdown.Item eventKey="Silver">Silver</Dropdown.Item>
+              <Dropdown.Item eventKey="Red">Red</Dropdown.Item>
+              <Dropdown.Item eventKey="Orange">Orange</Dropdown.Item>
+              <Dropdown.Item eventKey="Pink">Pink</Dropdown.Item>
             </DropdownButton>
           </Col>
           <Col md={3} className="g-0">
-            <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            <DropdownButton id="location-dropdown" title="Location" onSelect={function(evt){console.log(evt); setBuilding(evt);}}>
+            <Dropdown.Item eventKey="All">All</Dropdown.Item>
+            <Dropdown.Item eventKey="Building-1">Building 1</Dropdown.Item>
+            <Dropdown.Item eventKey="Building-2">Building 2</Dropdown.Item>
+            <Dropdown.Item eventKey="Building-3">Building 3</Dropdown.Item>
+            <Dropdown.Item eventKey="Building-4">Building 4</Dropdown.Item>
+            <Dropdown.Item eventKey="Building-5">Building 5</Dropdown.Item>
             </DropdownButton>
           </Col>
         </Row>
